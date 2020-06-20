e33f3a4ded4898f0577a41df1dbf0ea7731f9b8e
Nishant <nishant1994garg@gmail.com>
2020-06-20 12:25:32 +0530

    Add review create

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,10 +1,21 @@
 const Post = require('../models/post');
-const review = require('../models/review');
+const Review = require('../models/review');
 
 module.exports = {
 	// Reviews Create
 	async reviewCreate(req, res, next){
-		
+		// find the post by id
+		let post = await Post.findById(req.params.id);
+		// create
+		//req.body.review.author = req.user._id;
+		let review = await Review.create(req.body.review);
+		// assign
+		post.reviews.push(review);
+		// save
+		post.save();
+		// redirect
+		req.session.success = 'Review created successfully!';
+		res.redirect(`/posts/${post.id}`);
 	},
 	// Reviews Update
 	async reviewUpdate(req, res, next){
